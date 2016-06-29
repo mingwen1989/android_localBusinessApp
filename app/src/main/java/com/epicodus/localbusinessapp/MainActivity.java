b09087e62a033719e6a6bdbb034df2b9cf6cfc90
John Klein and Ming Wen <johnmbklein@gmail.com>
2016-06-29 10:26:57 -0700

    Added Picasso

--- app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
@@ -6,14 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.menuButton) Button mMenuButton;
-    private TextView mAppNameTextView;
+    @Bind (R.id.textView) TextView mAppNameTextView;
+    @Bind(R.id.galleryImageView) ImageView mGalleryImageView;
 
 
     @Override
@@ -21,10 +25,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mAppNameTextView = (TextView) findViewById(R.id.textView);
+
         Typeface aarghFont = Typeface.createFromAsset(getAssets(), "fonts/Aaargh.ttf");
         mAppNameTextView.setTypeface(aarghFont);
         mMenuButton.setOnClickListener(this);
+
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mGalleryImageView);
     }
 
     @Override
--- app/src/main/java/com/epicodus/localbusinessapp/MenuActivity.java
+++ app/src/main/java/com/epicodus/localbusinessapp/MenuActivity.java
@@ -3,13 +3,32 @@ package com.epicodus.localbusinessapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MenuActivity extends AppCompatActivity {
+    @Bind(R.id.listView) ListView mListView;
+
+    private ArrayList<String> newArray = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+        newArray.add("warm shortcake w/ seasonal fruit");
+        newArray.add("glazed breakfast hushpuppies");
+        newArray.add("screen door praline bacon");
+        newArray.add("screen door fried chicken and waffle");
+
         Intent intent = getIntent();
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,newArray);
+        mListView.setAdapter(adapter);
     }
 }
