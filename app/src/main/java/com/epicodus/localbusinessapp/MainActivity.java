735cfb6321c6304c1482833d5c14894d89aa6e66
John Klein and Ming Wen <johnmbklein@gmail.com>
2016-06-29 11:03:25 -0700

    Add grid layout with images

--- app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
@@ -16,8 +16,13 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.menuButton) Button mMenuButton;
-    @Bind (R.id.textView) TextView mAppNameTextView;
-    @Bind(R.id.galleryImageView) ImageView mGalleryImageView;
+    @Bind(R.id.imageView4) ImageView mImageOne;
+    @Bind(R.id.imageView5) ImageView mImageTwo;
+    @Bind(R.id.imageView6) ImageView mImageThree;
+    @Bind(R.id.imageView7) ImageView mImageFour;
+    @Bind(R.id.imageView8) ImageView mImageFive;
+    @Bind(R.id.imageView9) ImageView mImageSix;
+
 
 
     @Override
@@ -26,11 +31,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Typeface aarghFont = Typeface.createFromAsset(getAssets(), "fonts/Aaargh.ttf");
-        mAppNameTextView.setTypeface(aarghFont);
         mMenuButton.setOnClickListener(this);
 
-        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mGalleryImageView);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageOne);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageTwo);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageThree);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageFour);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageFive);
+        Picasso.with(this).load("http://s3-media1.fl.yelpcdn.com/bphoto/zIZHcwdQnaj1P-uRyhKU5Q/o.jpg").into(mImageSix);
+
     }
 
     @Override
