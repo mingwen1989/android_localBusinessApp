f861a0c637aecd686214ecfeee5194e372f58c48
John Klein and Ming Wen <Guest@epicodus-25.local>
2016-06-29 09:38:35 -0700

    Add ButterKnife and onClickListener interface

--- app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusinessapp/MainActivity.java
@@ -1,19 +1,35 @@
 package com.epicodus.localbusinessapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.menuButton) Button mMenuButton;
     private TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         mAppNameTextView = (TextView) findViewById(R.id.textView);
         Typeface aarghFont = Typeface.createFromAsset(getAssets(), "fonts/Aaargh.ttf");
         mAppNameTextView.setTypeface(aarghFont);
+        mMenuButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent);
     }
 }
